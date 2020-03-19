24a11df2728352027b06621f9c109a08b6e75a97
pshilin <58673233+pshilin@users.noreply.github.com>
2020-03-18 20:08:12 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("brave new world");
+        System.out.println("change one");
+            
     }
 }
