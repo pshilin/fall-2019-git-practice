448947dee2c70877a1fc41d400b441c292200930
pshilin <58673233+pshilin@users.noreply.github.com>
2020-03-25 19:54:33 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
-        System.out.println("brave new world");
+        System.out.println("brave new world"); //comment in GH
         System.out.println("change one");
         System.out.println("change two");
         System.out.println("change three");
